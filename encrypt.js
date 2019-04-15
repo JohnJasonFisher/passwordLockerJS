55a0dbf571c6f032b28c25fcb5cda396f29a8cdc
John Jason Fisher <jjfisher91@gmail.com>
2019-04-14 22:30:28 -0500

    adds_encrption_file

---
+++ encrypt.js
@@ -0,0 +1,16 @@
+import passwords from 'passwords.json'
+import fs from 'fs'
+import SimpleCrypto from "simple-crypto-js"
+
+const key = SimpleCrypto.generateRandom()
+console.log(`This is your new encrption key:
+${key}`)
+const simpleCryptoObj = new SimpleCrypto(key)
+
+for (let props in passwords) {
+    passwords[props].password = simpleCryptoObj.encrypt(passwords[props].password)
+}
+
+const newPasswords = JSON.stringify(passwords, null, '\t')
+        
+fs.writeFileSync("passwords.json", newPasswords)

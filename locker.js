ee0c3e5ee776aa0d143f929b3a0053c854788722
JohnJasonFisher <jjfisher91@gmail.com>
2019-10-07 16:14:24 -0500

    Use plain JS

--- locker.js
+++ locker.js
@@ -1,21 +1,11 @@
-#!/usr/bin/env node
-
-import passwords from './passwords.json'
-import copyPaste from 'copy-paste'
-import dotenv from 'dotenv'
-import SimpleCrypto from "simple-crypto-js"
-
-dotenv.config();
-
-const _secretKey = process.env.KEY
- 
-const simpleCrypto = new SimpleCrypto(_secretKey);
+const passwords = require('./passwords.json')
+const copyPaste = require('copy-paste')
 
 const inquiry = process.argv[2]
 
 if (passwords.hasOwnProperty(inquiry)) {
     const username = passwords[inquiry]['username']
-    const password = simpleCrypto.decrypt(passwords[inquiry]['password'])
+    const password = passwords[inquiry]['password']
 
     console.log(`Your username is ${username}`)
     console.log(`Your password is ${password}`)
